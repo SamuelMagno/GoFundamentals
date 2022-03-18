8425f9f1de835d672047472adcb875ec6323f73b
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-17 21:37:12 -0300

    Create first go code

---
+++ 12/primeiro.go
@@ -0,0 +1,11 @@
+//Programas executáveis se iniciam pelo pacote main
+package main
+
+import "fmt"
+
+//Os códigos em GO são organizados em pacotes
+//para usá-los é necessário declarar um ou vários imports
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
