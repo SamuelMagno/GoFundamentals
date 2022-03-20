a6f00fa57b71e40f96306f8dc86ebb633a6eec69
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-20 14:24:10 -0300

    pointers example

--- ponteiro/ponteiro.go
+++ ponteiro/ponteiro.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	i := 1
+	var p *int = nil
+
+	p = &i //&i get i address to p
+	*p++   //*p get value
+
+	fmt.Println("p equals to", *p)
+	fmt.Println("i equals to", i)
+	fmt.Println("p address", p)
+	fmt.Println("i address", &i)
+
+	//Go não permite aritmética de ponteiros
+	//p++
 }
