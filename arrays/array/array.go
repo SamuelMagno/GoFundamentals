65ca628074efbbd3a560d38e1f8608fcb037b9c4
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-27 16:45:12 -0300

    array basics

---
+++ arrays/array/array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notas [3]float64
+
+	notas[0], notas[1], notas[2] = 7.8, 9.1, 6.7
+	fmt.Println(notas)
+
+	var total float64 = 0
+
+	for i := 0; i < len(notas); i++ {
+		total += notas[i]
+	}
+
+	media := total / float64(len(notas))
+	fmt.Printf("Média %.2f", media)
+}
