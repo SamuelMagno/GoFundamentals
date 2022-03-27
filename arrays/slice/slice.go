c07cbc27ce87e8223cd1dcf067e34b681d6feeb3
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-27 17:04:35 -0300

    slice structures

--- arrays/slice/slice.go
+++ arrays/slice/slice.go
@@ -1,1 +1,26 @@
-package slice
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a1 := [3]int{1, 2, 3} //array
+	s1 := []int{1, 2, 3}  //slice
+	fmt.Println(a1, s1)
+	fmt.Println(reflect.TypeOf(a1), reflect.TypeOf(s1))
+
+	a2 := [5]int{1, 2, 3, 4, 5}
+	//slice define um pedaço de array
+
+	s2 := a2[1:3]
+	fmt.Println(a2, s2)
+
+	s3 := a2[:2]
+	fmt.Println(s3)
+
+	//slice possui um ponteiro para determinado index de um array
+	s4 := s2[:1]
+	fmt.Println(s4)
+}
