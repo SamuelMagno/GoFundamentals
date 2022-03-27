a5d43f67aa0d0af05efec0c9b519b9d5f0bb4c8d
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-27 14:16:28 -0300

    Adding for examples

--- controlStructure/for/for.go
+++ controlStructure/for/for.go
@@ -1,1 +1,25 @@
-package _for
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 1
+
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Println(i)
+		i++
+	}
+
+	for {
+		fmt.Println("Infinite loop")
+		time.Sleep(time.Second)
+	}
+}
