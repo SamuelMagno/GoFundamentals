352237f73e0cf25dc5135aaecce7c97db9e4c92d
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-28 20:27:11 -0300

    append and copy methods

--- arrays/appendcopy/appendcopy.go
+++ arrays/appendcopy/appendcopy.go
@@ -1,3 +1,16 @@
 package main
 
-func main()
+import "fmt"
+
+func main() {
+	array1 := [3]int{1, 2, 3}
+	var slice1 []int
+
+	//array1 = append(array1,  4, 5, 6) append only work with slices
+	slice1 = append(slice1, 4, 5, 6)
+	fmt.Println(array1, slice1) // [1,2,3][4,5,6]
+
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice2) // [4, 5]
+}
