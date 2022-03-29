356ef6093421a498cd97aae5e895a1e6cd478211
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-28 21:14:12 -0300

    map examples

--- arrays/map2/map.go
+++ arrays/map2/map.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	funcSalario := map[string]float64{
+		"José":    1110.45,
+		"Gabriel": 3232.90,
+		"Pedro":   2323.89,
+	}
+	funcSalario["Rafael"] = 1345.0
+
+	fmt.Println(funcSalario)
+	delete(funcSalario, "Null")
+
+	for nome, salario := range funcSalario {
+		fmt.Println(nome, salario)
+	}
 }
