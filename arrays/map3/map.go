8441bb93fcd091f44a2767d260340adcca41a6e0
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-28 21:18:13 -0300

    nested maps

--- arrays/map3/map.go
+++ arrays/map3/map.go
@@ -1,1 +1,25 @@
-package map3
+package main
+
+import "fmt"
+
+func main() {
+	funcPorLetra := map[string]map[string]float64{
+		"G": {
+			"Gabriel": 1321.78,
+			"Gilson":  5434.90,
+		},
+		"J": {
+			"Jose": 1232.54,
+			"Juan": 5090.90,
+		},
+		"P": {
+			"Paula": 6312.90,
+			"Pedro": 2321.90,
+		},
+	}
+
+	delete(funcPorLetra, "J")
+	for letra, funcs := range funcPorLetra {
+		fmt.Println(letra, funcs)
+	}
+}
