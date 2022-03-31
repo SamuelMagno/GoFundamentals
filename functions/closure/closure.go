8627192889c9c5b7b9b99141b0e509c102f6487d
Samuel Magno <samuelrtfmagno@gmail.com>
2022-03-30 22:06:18 -0300

    function closures

--- functions/closure/closure.go
+++ functions/closure/closure.go
@@ -1,1 +1,20 @@
-package closure
+package main
+
+import "fmt"
+
+func closure() func() {
+	x := 10
+	var funcao = func() {
+		fmt.Println(x)
+	}
+	return funcao
+}
+
+func main() {
+	x := 20
+	fmt.Println(x)
+
+	printX := closure()
+
+	printX()
+}
